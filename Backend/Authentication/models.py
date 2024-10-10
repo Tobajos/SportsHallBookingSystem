da036bd202e776e06dbeed2e9861d37b76462338
Tobiasz Podlesny <tobiasz.podlesny@gmail.com>
2024-10-10 18:36:16 +0200

    Initialize Django project and implement CustomUser model

---
+++ Backend/Authentication/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractUser
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError("User must have an email address!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_staffuser(self,email,firstname=None,lastname=None,password=None):
+        staff_user = self.create_user(firstname=firstname,lastname=lastname,email=email,password=password,staff=True)
+        return staff_user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+    
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True,null=False,max_length=100)
+    firstname = models.CharField(null=True,max_length=60,default='')
+    lastname = models.CharField(null=True,max_length=60,default='')
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True,null = False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    
+       
+    def __str__(self):
+        return self.email
